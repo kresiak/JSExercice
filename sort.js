a95529ef49defb4b14e9cdaa977517d2ff1e3dea
kresiak <kresiak1@gmail.com>
2017-03-03 23:38:17 +0100

    sort ()

---
+++ sort.js
@@ -0,0 +1,19 @@
+
+var dbStrings = ['zero', 'alex', 'merci', 'abc']
+    var sortedStrings = dbStrings.sort();
+
+var dbStrings2 = ['zero', 'alex', 'merci', 'abc']
+    var sortedStrings2 = dbStrings2.sort();
+    var sortedStringsReverse = dbStrings2.reverse();
+
+var dbNumbers =  [12, 4, 2, 8, 5, 1]
+    var sortedNumbers = dbNumbers.sort((a, b) => a - b);
+
+var dbNumbers2 =  [12, 4, 2, 8, 5, 1]
+    var sortedNumbersInverted = dbNumbers2.sort((a, b) => b - a);
+
+var dbObjects =  [{nom:'alex', age:25, lieu:'Paris'},  {nom:'paul', age:12, lieu:'Londres'}, {nom:'henri', age:20, lieu:'Varsovie'}]
+    var sortedObjects = dbObjects.sort((a, b) => a.age - b.age);
+
+var dbObjects2 =  [{nom:'alex', age:25, lieu:'Paris'},  {nom:'paul', age:12, lieu:'Londres'}, {nom:'henri', age:20, lieu:'Varsovie'}]
+    var sortedObjectsInverted = dbObjects2.sort((a, b) => b.age - a.age);
