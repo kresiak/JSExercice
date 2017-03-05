69587dc93dee2a96d3c8803cab3b0a49b8508ac6
kresiak <kresiak1@gmail.com>
2017-03-05 20:58:11 +0100

    sort ()

--- sort.js
+++ sort.js
@@ -3,8 +3,8 @@ var dbStrings = ['zero', 'alex', 'merci', 'abc']
     var sortedStrings = dbStrings.sort();
 
 var dbStrings2 = ['zero', 'alex', 'merci', 'abc']
-    var sortedStrings2 = dbStrings2.sort();
-    var sortedStringsReverse = dbStrings2.reverse();
+    var sortedStrings2 = dbStrings2.sort((a, b) => 
+    {return a.toLowerCase() < b.toLowerCase()});
 
 var dbNumbers =  [12, 4, 2, 8, 5, 1]
     var sortedNumbers = dbNumbers.sort((a, b) => a - b);
